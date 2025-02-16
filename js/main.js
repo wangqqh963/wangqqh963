5872570f5222c329eca9511ce31e51700f838615
nanaall96 <2539834033@qq.com>
2025-02-16 16:24:00 +0800

    Site updated: 2025-02-16 16:24:00

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// Google监控 https://analytics.google.com/
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','G-5LVB7LS3TC');
-
-//Microsoft监控 https://clarity.microsoft.com/
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "qa5xzi2mms");
-
 const app = Vue.createApp({
     mixins: Object.values(mixins),
     data() {
